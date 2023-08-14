a62336e20e3733392bf21c27889aea4d45203120
samuel_code <137518982@qq.com>
2023-08-14 16:04:03 +0800

    modify pwd_app /views.py/ time -> time.asctime(time.localtime(time.time()))

--- apps/pwd_app/views.py
+++ apps/pwd_app/views.py
@@ -25,7 +25,7 @@ class UpdatePWD(APIView):
                 log_msg = {
                     'info': '用户ad信息获取失败',
                     'user': ''.format(ad_account),
-                    'time': ''.format(time)
+                    'time': ''.format(time.asctime(time.localtime(time.time())))
                 }
                 logger_user.error(json.dumps(log_msg,ensure_ascii=False))
                 res = {
@@ -37,22 +37,37 @@ class UpdatePWD(APIView):
             #修改密码:
             try:
                 conn.extend.microsoft.modify_password(account_ou, old_password=pre_pwd, new_password=current_pwd)
-                log_msg = {
-                    'info':'修改密码成功',
-                    'user':'用户'.format(ad_account),
-                    'time':''.format(time)
-                }
-                res = {
-                    'code':200,
-                    'info':'修改密码成功'
-                }
-                logger_user.info(json.dumps(log_msg,ensure_ascii=False))
-                return JsonResponse(res)
+                result = conn.result
+                if result['result'] == 0:
+                    conn.unbind()
+                    log_msg = {
+                        'info':'修改密码成功',
+                        'user':'用户'.format(ad_account),
+                        'time':''.format(time.asctime(time.localtime(time.time())))
+                    }
+                    res = {
+                        'code':200,
+                        'info':'修改密码成功'
+                    }
+                    logger_user.info(json.dumps(log_msg,ensure_ascii=False))
+                    return JsonResponse(res)
+                else:
+                    log_msg = {
+                        'info': '修改密码失败',
+                        'user': '用户'.format(ad_account),
+                        'time': ''.format(time.asctime(time.localtime(time.time())))
+                    }
+                    res = {
+                        'code': 500,
+                        'info': '修改密码失败'
+                    }
+                    logger_user.info(json.dumps(log_msg, ensure_ascii=False))
+                    return JsonResponse(res)
             except Exception as e :
                 log_msg = {
                     'info': '修改密码失败',
                     'user': '用户名',
-                    'time': ''.format(),
+                    'time': ''.format(time.asctime(time.localtime(time.time()))),
                     'detail': '异常信息：{}'.format(sys.exc_info()[0]),
                 }
                 logger_user.error(json.dumps(log_msg, ensure_ascii=False))
@@ -64,7 +79,7 @@ class UpdatePWD(APIView):
         except Exception as e :
             log_msg = {
                 'info':'AD初始化失败',
-                'time':''.format(),
+                'time':''.format(time.asctime(time.localtime(time.time()))),
                 'detail': '异常信息：{}'.format(sys.exc_info()[0]),
             }
             logger_user.error(json.dumps(log_msg,ensure_ascii=False))
